7627761395da5e3378f10b72aabb273dc7f5cae6
Gergo <gergomolnar19@gmail.com>
2018-04-30 20:35:16 +0200

    Jatekos nevbekeres

--- Torpedo/Main.cpp
+++ Torpedo/Main.cpp
@@ -3,9 +3,16 @@
 using namespace std;
 
 int main(){
+	char nev1[10];
 	Tabla tabla1(10);
 
+	cout << "Adja meg az elso jatekos nevet:\t";
+	cin >> nev1;
+	system("cls");
+	cout << nev1 << " sajat tablaja:\n\n";
 	tabla1.kiRajzol();
+
+	
 
 	return 0;
 }
